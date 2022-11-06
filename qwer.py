672019c1c45d250517fb219a9ac1974858777d68
정수철 <sudo.g41@gmail.com>
2022-11-06 21:45:35 +0900

    [..?] ?? qwer...???

--- qwer.py
+++ qwer.py
@@ -1,4 +1,8 @@
 from src import ytdl
+from time import strftime, localtime
+
+a = strftime('%Y. %m. %d. %H:%M:%S(%a) KST')
+print(a, type(a))
 
 # ytdl.download_audio_only("https://youtu.be/CupFQnoO_28")
 
